f1f6d9a0ad1ad43105fe0f4f13b8d0453e239691
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-02-18 18:23:33 +0100

    Stworzenie elementow widokow

--- src/controller/FilterController.java
+++ src/controller/FilterController.java
@@ -3,22 +3,69 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import managers.ImageManager;
 
 public class FilterController implements Initializable {
 
 	private ImageManager imageManager = new ImageManager();
-	
+
+	@FXML
+	private Button filterButton;
+
+	@FXML
+	private Button clearFilterButton;
+
+	@FXML
+	private ChoiceBox<?> minorChoiceBox;
+
+	@FXML
+	private ChoiceBox<?> majorChoiceBox;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+
 		System.out.println("Inicjalizacja kontroleru filtru");
-		
+
 	}
 
 	public void setImageManager(ImageManager imageManager) {
 		this.imageManager = imageManager;
 	}
-	
+
+	private Button getFilterButton() {
+		return filterButton;
+	}
+
+	private void setFilterButton(Button filterButton) {
+		this.filterButton = filterButton;
+	}
+
+	private Button getClearFilterButton() {
+		return clearFilterButton;
+	}
+
+	private void setClearFilterButton(Button clearFilterButton) {
+		this.clearFilterButton = clearFilterButton;
+	}
+
+	private ChoiceBox<?> getMinorChoiceBox() {
+		return minorChoiceBox;
+	}
+
+	private void setMinorChoiceBox(ChoiceBox<?> minorChoiceBox) {
+		this.minorChoiceBox = minorChoiceBox;
+	}
+
+	private ChoiceBox<?> getMajorChoiceBox() {
+		return majorChoiceBox;
+	}
+
+	private void setMajorChoiceBox(ChoiceBox<?> majorChoiceBox) {
+		this.majorChoiceBox = majorChoiceBox;
+	}
+
 }
--- src/controller/ImagesController.java
+++ src/controller/ImagesController.java
@@ -3,12 +3,33 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import managers.ImageManager;
 
 public class ImagesController implements Initializable {
 	
 	private ImageManager imageManager = new ImageManager();
+	
+	@FXML
+    private ImageView mainImageView;
+
+    @FXML
+    private ImageView firstMinorImageView;
+
+    @FXML
+    private ImageView secondMinorImageView;
+
+    @FXML
+    private ImageView thirdMinorImageView;
+
+    @FXML
+    private Button previousImageButton;
+
+    @FXML
+    private Button nextImageButton;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -19,6 +40,54 @@ public class ImagesController implements Initializable {
 
 	public void setImageManager(ImageManager imageManager) {
 		this.imageManager = imageManager;
+	}
+
+	private ImageView getMainImageView() {
+		return mainImageView;
+	}
+
+	private void setMainImageView(ImageView mainImageView) {
+		this.mainImageView = mainImageView;
+	}
+
+	private ImageView getFirstMinorImageView() {
+		return firstMinorImageView;
+	}
+
+	private void setFirstMinorImageView(ImageView firstMinorImageView) {
+		this.firstMinorImageView = firstMinorImageView;
+	}
+
+	private ImageView getSecondMinorImageView() {
+		return secondMinorImageView;
+	}
+
+	private void setSecondMinorImageView(ImageView secondMinorImageView) {
+		this.secondMinorImageView = secondMinorImageView;
+	}
+
+	private ImageView getThirdMinorImageView() {
+		return thirdMinorImageView;
+	}
+
+	private void setThirdMinorImageView(ImageView thirdMinorImageView) {
+		this.thirdMinorImageView = thirdMinorImageView;
+	}
+
+	private Button getPreviousImageButton() {
+		return previousImageButton;
+	}
+
+	private void setPreviousImageButton(Button previousImageButton) {
+		this.previousImageButton = previousImageButton;
+	}
+
+	private Button getNextImageButton() {
+		return nextImageButton;
+	}
+
+	private void setNextImageButton(Button nextImageButton) {
+		this.nextImageButton = nextImageButton;
 	}
 	
 }
